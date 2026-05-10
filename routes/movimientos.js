79088373b93f5a497e2e3c495a7d2dd04ef093f4
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2026-05-10 14:03:44 +0200

    Rutas CRUD de movimientos
    Se han hecho los métodos CRUD para las rutas de los movimientos

---
+++ routes/movimientos.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const movimientos = require("../data/movimientos");
+
+// ─────────────────────────────────────────────
+// GET /api/movimientos
+// Devuelve todos los movimientos.
+// ─────────────────────────────────────────────
+router.get("/", (req, res) =>
+    {
+        return res.status(200).json(movimientos);
+    }
+);
+
+// ─────────────────────────────────────────────
+// GET /api/movimientos/pokemon/1
+// Devuelve los movimientos del pokémon en concreto.
+// ─────────────────────────────────────────────
+router.get("/pokemon/:pokemon_id", (req, res) => 
+    {
+        const movs = movimientos.filter(m => m.pokemon_id == req.params.pokemon_id);    // filter() reccorre todo el array y devuelve todos los que coincidan
+        if(movs.length === 0)   // Si el array está vacío y no se encontró nada
+            return res.status(404).json({ error: 'No se encontraron movimientos para el pokémon con id ${req.params.pokemon_id}' });
+
+        return res.status(200).json(movs);
+    }
+);
+
+// ─────────────────────────────────────────────
+// POST /api/movimientos
+// Crear un nuevo movimiento.
+// ─────────────────────────────────────────────
+router.post("/" , (req, res) => 
+    {
+        const { nombre, tipo, potencia, precision, pokemon_id } = req.body;
+
+        if(!nombre || !tipo || !potencia || !precision || !pokemon_id)
+        {
+            return res.status(400).json({ error: "Faltan campos obligatorios: nombre, tipo, potencia, precision, pokemon_id" });
+        }
+
+        const nuevoMovimiento =
+        {
+            id: movimientos.length + 1,
+            nombre,
+            tipo,
+            potencia,
+            precision,
+            pokemon_id
+        };
+
+        movimientos.push(nuevoMovimiento);
+        return res.status(201).json(nuevoMovimiento);
+    }
+);
+
+// ─────────────────────────────────────────────
+// DELETE /api/movimientos/1
+// Eliminar un movimiento
+// ─────────────────────────────────────────────
+router.delete("/:id", (req, res) => 
+    {
+        const index = movimientos.findIndex(m => m.id == req.params.id);
+        if(index === -1)
+            return res.status(404).json({ error: 'No existe ningún movimiento con id ${}req.params.id' });
+        
+        const eliminado = movimientos.splice(index, 1);
+        return res.status(200).json({ mensaje: 'Movimiento ${eliminado[0].nombre} eliminado correctamente' });
+    }
+);
+
+module.exports = router;
